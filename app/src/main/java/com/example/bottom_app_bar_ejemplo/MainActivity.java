2b762a9607399544126aeb8d82473bff7adb9ab3
Miguel Fernando Marlés Betancourt <80717402+ferchitoM@users.noreply.github.com>
2024-08-16 21:57:54 -0500

    update

--- app/src/main/java/com/example/bottom_app_bar_ejemplo/MainActivity.java
+++ app/src/main/java/com/example/bottom_app_bar_ejemplo/MainActivity.java
@@ -15,6 +15,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         // Configurar barra de herramientas
         MaterialToolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle("");
 
         // Configurar título de la barra de herramientas
         TextView toolbarTitle = findViewById(R.id.toolbar_title);
@@ -34,11 +36,15 @@ public class MainActivity extends AppCompatActivity {
         // Configurar botón de menú de la barra de herramientas
         ImageButton menuButton = findViewById(R.id.boton_bar_menu);
 
-        menuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showPopUp(v);
-            }
+        menuButton.setOnClickListener(v -> {
+            showPopUp(v);
+        });
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnItemSelectedListener(item -> {
+
+            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            return true;
         });
 
     }
@@ -48,14 +54,12 @@ public class MainActivity extends AppCompatActivity {
         popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
 
         // Configurar acciones para los elementos del menú emergente
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                // Lógica para manejar las acciones de los elementos del menú emergente
-                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
-                return true;
-            }
+        popupMenu.setOnMenuItemClickListener(item -> {
+
+            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            return true;
         });
+
         // Mostrar el menú emergente
         popupMenu.show();
     }
